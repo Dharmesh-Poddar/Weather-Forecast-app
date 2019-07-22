a9fab049874e975c4737a994052d92c97942d398
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-07-22 21:57:08 +0530

    fetching api

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ let API_Key= "4d5e7c1a2cd8f2338eb3f0d0b21ce3b0";
 class App extends React.Component{
     getWeather =async(e) =>{
                e.preventDefault();
-      const city= e.target.elements.name.city.value;
-      const country =e.target.elements.name.country.value;
+      const city= e.target.elements.city.value;
+      const country =e.target.elements.country.value;
 
 
       const api_call =await fetch('http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_Key}');
        
         const data =await api_call.json();
         console.log(data);
-        
+
 
     }
    
